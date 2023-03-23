b749856a8d78861fb29927f8345e5e6bb52f7ffd
alex <durandaj027@gmail.com>
2023-03-23 15:08:45 -0400

    bug fixed on permanence

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,19 +4,23 @@ import Home from "./commons/Home";
 import FormRegister from "./ components/Register";
 import "bootstrap/dist/css/bootstrap.min.css";
 import axios from "axios";
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { setUser } from "./state/user";
 
 function App() {
   const dispatch = useDispatch();
+  const userLoged = useSelector((state) => state.user);
+
   useEffect(() => {
     axios
       .get("http://localhost:3001/api/user/me", { withCredentials: true })
       .then((user) => {
-        console.log(user.data.user), dispatch(setUser(user.data.user));
+        if (!userLoged.name) {
+          console.log(user.data.user), dispatch(setUser(user.data.user));
+        }
       });
-  });
+  }, [userLoged]);
   return (
     <div>
       <Routes>
