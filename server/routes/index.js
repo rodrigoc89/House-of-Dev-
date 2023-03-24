c5e08e4b8b34893868aae4fe604f55ab64b88ad4
rodrigoc89 <rodrigojoaquinc89@gmail.com>
2023-03-24 13:10:10 -0300

    add route admin

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
 
 const userRoute = require("./user");
+const adminRoute = require("./admin");
 
 router.use("/user", userRoute);
+router.use("/admin", adminRoute);
 
 module.exports = router;
--- server/routes/user.js
+++ server/routes/user.js
@@ -5,6 +5,10 @@ const User = require("../models/User");
 const { generateToken } = require("../config/token");
 const { validateAuth } = require("../middleware/auth");
 
+router.get("/me", validateAuth, (req, res) => {
+  res.send(req.user);
+});
+
 router.post("/register", (req, res) => {
   const { name, lastName, password, email } = req.body;
   const newUser = {
@@ -42,39 +46,5 @@ router.post("/logout", (req, res) => {
   res.clearCookie("token");
   res.status(204).send("logout");
 });
-
-// ADMIN
-
-router.put("/admin/:id", validateAuth, (req, res) => {
-  const { id } = req.params;
-  User.findByPk(id).then((user) => {
-    console.log(user);
-    user
-      .update(req.body, { where: { id: id } })
-      .then((userUpdate) => res.status(201).send(userUpdate))
-      .catch((error) => console.log(error));
-  });
-});
-
-router.get("/admin/:id", (req, res) => {
-  const { id } = req.params;
-  User.findByPk(id)
-    .then((user) => {
-      res.status(201).send(user);
-    })
-    .catch((error) => console.log(error));
-});
-
-router.get("/adminList", (req, res) => {
-  User.findAll()
-    .then((users) => {
-      res.status(201).send(users);
-    })
-    .catch((error) => console.log(error));
-});
-
-router.get("/me", validateAuth, (req,res)=>{
-  res.send(req.user)
-})
 
 module.exports = router;
