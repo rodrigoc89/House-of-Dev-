4894a52af8c5c113d0b6464b79c00dc5537ea6f2
rodrigoc89 <rodrigojoaquinc89@gmail.com>
2023-03-31 10:22:40 -0300

    connection routes and fix bugs

--- client/src/ components/admin/ModalProperty.jsx
+++ client/src/ components/admin/ModalProperty.jsx
@@ -1,16 +1,51 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Form from "react-bootstrap/Form";
+import axios from "axios";
 
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 
-function ModalProperty() {
+function ModalProperty({ id }) {
   const [show, setShow] = useState(false);
   const [validated, setValidated] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+  const [property, setProperty] = useState([]);
+  const [address, setAddress] = useState("");
+  const [bathrooms, setBathrooms] = useState("");
+  const [bedrooms, setBedrooms] = useState("");
+  const [price, setPrice] = useState("");
+  const [m2, setM2] = useState("");
+  const [image, setImage] = useState("");
+  // const [options, setOptions] = useState("");
+  const [description, setDescription] = useState("");
+
   const handleSubmit = (event) => {
-    const form = event.currentTarget;
+    const property = {
+      address: address,
+      bathrooms: bathrooms,
+      bedrooms: bedrooms,
+      price: price,
+      m2: m2,
+      image: image,
+      // options: options,
+      available: true,
+      description: description,
+    };
+    event.preventDefault();
+    axios
+      .put(`http://localhost:3001/api/property/${id}`, property, {
+        withCredentials: true,
+      })
+      .then(() => console.log("actualized"));
+  };
+  const handleClick = (event) => {
+    axios
+      .get(`http://localhost:3001/api/property/${id}`, {
+        withCredentials: true,
+      })
+      .then((house) => setProperty([house.data]));
+    const form = event.currentTargett;
     if (form.checkValidity() === false) {
       event.preventDefault();
       event.stopPropagation();
@@ -18,7 +53,6 @@ function ModalProperty() {
 
     setValidated(true);
   };
-
   return (
     <div
       className="modal show"
@@ -38,75 +72,120 @@ function ModalProperty() {
           <Modal.Title>Edit Property</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <Form noValidate validated={validated} onSubmit={handleSubmit}>
-            <Form.Group className="mb-3" controlId="formBasicAddress">
-              <Form.Label>Address</Form.Label>
-              <Form.Control type="text" defaultValue="CALLE FALSA" required />
-              <Form.Control.Feedback type="invalid">
-                Please provide a valid address.
-              </Form.Control.Feedback>
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicBathrooms">
-              <Form.Label>Bathrooms</Form.Label>
-              <Form.Control type="number" defaultValue="3" required />
-              <Form.Control.Feedback type="invalid">
-                Please provide a valid numb of Bathrooms.
-              </Form.Control.Feedback>
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicBedrooms">
-              <Form.Label>Bedrooms</Form.Label>
-              <Form.Control type="number" defaultValue="4" required />
-              <Form.Control.Feedback type="invalid">
-                Please provide a valid Bedrooms.
-              </Form.Control.Feedback>
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicPrice">
-              <Form.Label>Price</Form.Label>
-              <Form.Control type="number" defaultValue="5000" required />
-              <Form.Control.Feedback type="invalid">
-                Please provide a valid Price.
-              </Form.Control.Feedback>
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicDescription">
-              <Form.Label>Description</Form.Label>
-              <Form.Control type="text" defaultValue="Description" required />
-              <Form.Control.Feedback type="invalid">
-                Please provide a valid Description.
-              </Form.Control.Feedback>
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicM²">
-              <Form.Label>m²</Form.Label>
-              <Form.Control type="number" defaultValue="153" required />
-              <Form.Control.Feedback type="invalid">
-                Please provide a valid value.
-              </Form.Control.Feedback>
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicOptions">
-              <Form.Label>Options</Form.Label>
-              <Form.Check
-                feedback="You must select at option before submitting."
-                type="checkbox"
-                label="Rent"
-                value="Rent"
-              />
-              <Form.Check
-                feedback="You must select at option before submitting."
-                type="checkbox"
-                label="Sale"
-                value="Sale"
-              />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicImage">
-              <Form.Label>Image</Form.Label>
-              <Form.Control type="text" defaultValue="url-image" required />
-              <Form.Control.Feedback type="invalid">
-                Please provide a valid url image.
-              </Form.Control.Feedback>
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicCheckbox">
-              <Form.Label>Available</Form.Label>
-              <Form.Check type="checkbox" value={true} />
-            </Form.Group>
+          <Form
+            noValidate
+            validated={validated}
+            onClick={handleClick}
+            onSubmit={handleSubmit}
+          >
+            {property.map((house) => (
+              <>
+                <Form.Group className="mb-3" controlId="formBasicAddress">
+                  <Form.Label>Address</Form.Label>
+                  <Form.Control
+                    onChange={(e) => setAddress(e.target.value)}
+                    type="text"
+                    defaultValue={house.address}
+                    required
+                  />
+                  <Form.Control.Feedback type="invalid">
+                    Please provide a valid address.
+                  </Form.Control.Feedback>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicBathrooms">
+                  <Form.Label>Bathrooms</Form.Label>
+                  <Form.Control
+                    onChange={(e) => setBathrooms(e.target.value)}
+                    type="number"
+                    defaultValue={house.bathrooms}
+                    required
+                  />
+                  <Form.Control.Feedback type="invalid">
+                    Please provide a valid numb of Bathrooms.
+                  </Form.Control.Feedback>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicBedrooms">
+                  <Form.Label>Bedrooms</Form.Label>
+                  <Form.Control
+                    onChange={(e) => setBedrooms(e.target.value)}
+                    type="number"
+                    defaultValue={house.bedrooms}
+                    required
+                  />
+                  <Form.Control.Feedback type="invalid">
+                    Please provide a valid Bedrooms.
+                  </Form.Control.Feedback>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicPrice">
+                  <Form.Label>Price</Form.Label>
+                  <Form.Control
+                    onChange={(e) => setPrice(e.target.value)}
+                    type="number"
+                    defaultValue={house.price}
+                    required
+                  />
+                  <Form.Control.Feedback type="invalid">
+                    Please provide a valid Price.
+                  </Form.Control.Feedback>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicDescription">
+                  <Form.Label>Description</Form.Label>
+                  <Form.Control
+                    onChange={(e) => setDescription(e.target.value)}
+                    type="text"
+                    defaultValue={house.description}
+                    required
+                  />
+                  <Form.Control.Feedback type="invalid">
+                    Please provide a valid Description.
+                  </Form.Control.Feedback>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicM²">
+                  <Form.Label>m²</Form.Label>
+                  <Form.Control
+                    onChange={(e) => setM2(e.target.value)}
+                    type="number"
+                    defaultValue={house.m2}
+                    required
+                  />
+                  <Form.Control.Feedback type="invalid">
+                    Please provide a valid value.
+                  </Form.Control.Feedback>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicOptions">
+                  <Form.Label>Options</Form.Label>
+                  <Form.Check
+                    feedback="You must select at option before submitting."
+                    type="checkbox"
+                    label="Rent"
+                    value="Rent"
+                  />
+                  <Form.Check
+                    feedback="You must select at option before submitting."
+                    type="checkbox"
+                    label="Sale"
+                    value="Sale"
+                  />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicImage">
+                  <Form.Label>Image</Form.Label>
+                  <Form.Control
+                    onChange={(e) => setImage(e.target.value)}
+                    type="text"
+                    defaultValue={house.image}
+                    required
+                  />
+                  <Form.Control.Feedback type="invalid">
+                    Please provide a valid url image.
+                  </Form.Control.Feedback>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                  <Form.Label>Available</Form.Label>
+                  <Form.Check type="checkbox" value={true} />
+                </Form.Group>
+              </>
+            ))}
+
             <Modal.Footer>
               <Button variant="secondary" onClick={handleClose}>
                 Close
--- client/src/ components/admin/adminTablas.jsx
+++ client/src/ components/admin/adminTablas.jsx
@@ -2,6 +2,8 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import Table from "react-bootstrap/Table";
 import ModalABM from "./ModalABM";
+import ModalProperty from "./ModalProperty";
+import ModalUser from "./ModalUser";
 
 function TableAdmin() {
   const [users, setUsers] = useState([]);
@@ -78,6 +80,8 @@ function TableAdmin() {
             <th>Name</th>
             <th>Apellido</th>
             <th>Email</th>
+            <th>Editar</th>
+            <th>Eliminar</th>
           </tr>
         </thead>
         <tbody>
@@ -88,6 +92,22 @@ function TableAdmin() {
                 <td>{user.name}</td>
                 <td>{user.lastName}</td>
                 <td>{user.email}</td>
+                <td>
+                  <ModalUser />
+                </td>
+                <td>
+                  <svg
+                    style={{ marginLeft: "30%" }}
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    class="bi bi-trash3-fill"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z" />
+                  </svg>
+                </td>
               </tr>
             );
           })}
@@ -123,7 +143,7 @@ function TableAdmin() {
             fontWeight: "bolder",
           }}
         >
-          PANEL properties
+          PANEL PROPERTIES
         </h6>
       </div>
       <ModalABM />
@@ -132,7 +152,6 @@ function TableAdmin() {
           width: "69.5%",
           marginLeft: "13%",
           marginTop: "2%",
-          marginBottom: "5%",
           border: "3px solid #123AC8",
         }}
         striped
@@ -144,6 +163,7 @@ function TableAdmin() {
             <th>Baños</th>
             <th>Dormitorios</th>
             <th>Precio</th>
+            <th>EDITAR</th>
           </tr>
         </thead>
         <tbody>
@@ -155,6 +175,9 @@ function TableAdmin() {
                 <td>{property.bathrooms}</td>
                 <td>{property.bedrooms}</td>
                 <td>{property.price + " $"}</td>
+                <td>
+                  <ModalProperty id={property.id} />
+                </td>
               </tr>
             );
           })}
